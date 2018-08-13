0046c887a8cde02594a8d3f96fd0acc472d5bf9d
Cláudio Costa <claudio.costa@gfi.pt>
2018-08-13 16:38:13 +0100

    Updated version for test

---
+++ gfi-test-landing/gfi-test-landing/Models/TestViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace gfi_test_landing.Models
+{
+    public class TestViewModel
+    {
+        [Required]
+        [Display(Name = "Test Name")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        public string TestName { get; set; }
+
+        [Required]
+        [Display(Name = "Test Description")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        public string TestDescription { get; set; }
+
+        [Required]
+        [Display(Name = "Test Browser")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        public string TestBrowser { get; set; }
+    }
+}
