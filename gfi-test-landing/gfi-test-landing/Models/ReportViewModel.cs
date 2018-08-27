2b0a10a2e2b27d77304c1961b1c86fa354ef094f
Cláudio Costa <claudio.costa@gfi.pt>
2018-08-27 18:29:08 +0100

    vm

---
+++ gfi-test-landing/gfi-test-landing/Models/ReportViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace gfi_test_landing.Models
+{
+    public class ReportViewModel
+    {
+        [Required]
+        [Display(Name = "Date start")]
+        [StringLength(100, ErrorMessage = "Date must be 12 digits long", MinimumLength = 3)]
+        public string DateStart { get; set; }
+
+        [Required]
+        [Display(Name = "Date end")]
+        [StringLength(100, ErrorMessage = "Date must be 12 digits long", MinimumLength = 3)]
+        public string DateEnd { get; set; }
+
+        public string Status { get; set; }
+
+        public int NumberOfTests { get; set; }
+
+        public int Id { get; set; }
+    }
+}
