4f50c2a6c8ad1ca395a01454bba9a66439031bf4
Pedro Délcio <delcio.pedro@gfi.pt>
2018-09-04 15:03:52 +0100

    Api created

--- gfi-test-landing/API.GFI.TestLanding/Controllers/ProjectController.cs
+++ gfi-test-landing/API.GFI.TestLanding/Controllers/ProjectController.cs
@@ -12,7 +12,6 @@ namespace API.Api.Controllers
     {
         private testLandingEntities db = new testLandingEntities();
 
-
         [Route("api/GetUserProjects/{idUser}")]
         public List<ProjectModel> GetProjects(string userId)
         {
@@ -24,6 +23,8 @@ namespace API.Api.Controllers
             return ProjectList;
         }
 
+
+        //Builds tests 
         [Route("api/GetNumberPassedTests/{idProject}")]
         public int GetNumberPassedTests(int idProject)
         {
@@ -35,6 +36,8 @@ namespace API.Api.Controllers
             return pass;
         }
 
+
+        //Builds tests
         [Route("api/GetNumberFailedTests/{idProject}")]
         public int GetNumberFailedTests(int idProject)
         {
@@ -46,10 +49,11 @@ namespace API.Api.Controllers
             return failed;
         }
 
+
+        //Get the last 5 builds 
         [Route("api/GetDateLastBuilds/{idProject}")]
         public List<String> GetDateLastBuilds(int idProject)
         {
-
             var dateDesc = (from p in db.Project
                             join r in db.Report on p.id equals idProject
                             orderby r.date_end descending
@@ -58,6 +62,7 @@ namespace API.Api.Controllers
             return dateDesc;
         }
 
+        //Get the last 5 tests
         [Route("api/GetLastFiveTotal/{idProject}")]
         public List<int?> GetLastFiveTotal(int idProject)
         {
@@ -70,6 +75,42 @@ namespace API.Api.Controllers
      }
 
 
+        //Get last 5 passed tests
+        [Route("api/GetLastFivePassedTests/{idProject}")]
+        public List<string> GetLastFivePassedTests(int idProject)
+        {
+            var statusLastFivePass = (from p in db.Project
+                                      join r in db.Report on p.id equals idProject
+                                      orderby r.date_end descending
+                                      select r.pass_tests).Take(5).ToList();
 
-   }
+            return statusLastFivePass;
+        }
+
+
+        //Get the last 5 failed tests
+        [Route("api/GetLastFiveFailedTests/{idProject}")]
+        public List<long?> GetLastFiveFailedTests(int idProject)
+        {
+            var statusLastFiveTotal = (from p in db.Project
+                                       join r in db.Report on p.id equals idProject
+                                       orderby r.date_end descending
+                                       select r.total_tests).Take(5).ToList();
+
+            var statusLastFivePass = (from p in db.Project
+                                      join r in db.Report on p.id equals idProject
+                                      orderby r.date_end descending
+                                      select r.pass_tests).Take(5).ToList();
+
+            List<long?> statusLastFiveFail = new List<long?>();
+            for (int i = 0; i < 5; i++)
+            {
+                statusLastFiveFail.Add(statusLastFiveTotal[i] - Int64.Parse(statusLastFivePass[i]));
+            }
+
+            return statusLastFiveFail;
+        }
+        
+
+    }
 }
