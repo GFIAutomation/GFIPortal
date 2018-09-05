74a1ce3c6994e3f5ea63b961d260cbb779a2ef87
Pedro Délcio <delcio.pedro@gfi.pt>
2018-09-05 11:51:36 +0100

    AdminLTE3 added to project

--- gfi-test-landing/gfi-test-landing/App_Start/BundleConfig.cs
+++ gfi-test-landing/gfi-test-landing/App_Start/BundleConfig.cs
@@ -8,50 +8,70 @@ namespace gfi_test_landing
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-3.3.1.js"));
+                bundles.Add(new ScriptBundle("~/Js").Include(
+                          "~/content/plugins/jquery/jquery.min.js",
+                          "~/content/plugins/bootstrap/js/bootstrap.bundle.min.js",
+                          "~/content/plugins/bootstrap/js/bootstrap.bundle.js",
+                          "~/content/plugins/bootstrap/js/bootstrap.min.js",
+                          "~/content/plugins/slimScroll/jquery.slimscroll.min.js",
+                          "~/content/plugins/fastclick/fastclick.js",
+                          "~/content/js/adminlte.js",
+                          "~/content/js/adminlte.min.js",
+                          "~/content/js/demo.js",
+                          "~/content/js/chart.js",
+                          "~/content/build/js/AdminLTE.js",
+                          "~/content/build/js/ControlSidebar.js",
+                          "~/content/build/js/Layout.js",
+                          "~/content/build/js/PushMenu.js",
+                          "~/content/build/js/SiteSearch.js",
+                          "~/content/build/js/Treeview.js",
+                          "~/content/build/js/Widget.js",
+                          "~/content/build/npm/Plugins.js",
+                          "~/content/build/npm/Publish.js",
+                          "~/content/js/pages/dashboard.js",
+                          "~/content/js/pages/dashboard2.js",
+                          "~/content/js/pages/dashboard3.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                bundles.Add(new StyleBundle("~/Css").Include(
+                          "~/Content/css/adminlte.min.css",
+                          "~/Content/css/adminlte.css",
+                          "~/content/plugins/bootstrap/css/*.css"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/sCss").Include(
+                          "~/content/build/scss/*.scss"
+                         /* "~/content/build/scss/_alerts.scss",
+                          "~/content/build/scss/_bootstrap-variable.scss",
+                          "~/content/build/scss/_brand.scss",
+                          "~/content/build/scss/_.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",
+                          "~/content/build/scss/_404_500_erros.scss",*/));
 
-            bundles.Add(new ScriptBundle("~/Content/js").Include(
-                //"~/Content/js/vendor/jquery-2.1.4.min.js",
-                "~/Scripts/jquery-3.3.1.js",
-                     "~/Content/js/plugins.js",
-                     "~/Content/js/main.js",
-                     "~/Content/js/dashboard.js",
-                     "~/Content/js/widgets.js",
-                     "~/Content/js/dropdown.js"));
 
-                       bundles.Add(new ScriptBundle("~/Content/TableJS").Include("~/Content/js/lib/data-table/datatables.min.js",
-                       "~/Content/js/lib/data-table/dataTables.bootstrap.min.js",
-                       "~/Content/js/lib/data-table/dataTables.buttons.min.js",
-                       "~/Content/js/lib/data-table/buttons.bootstrap.min.js",
-                       "~/Content/js/lib/data-table/jszip.min.js",
-                       "~/Content/js/lib/data-table/pdfmake.min.js",
-                       "~/Content/js/lib/data-table/vfs_fonts.js",
-                       "~/Content/js/lib/data-table/buttons.html5.min.js",
-                       "~/Content/js/lib/data-table/buttons.print.min.js",
-                        "~/Content/js/lib/data-table/buttons.colVis.min.js",
-                         "~/Content/js/lib/data-table/datatables-init.js"
-                       ));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/css/normalize.css",
-                      "~/Content/css/bootstrap.min.css",
-                      "~/Content/css/font-awesome.min.css",
-                      "~/Content/css/themify-icons.css",
-                      "~/Content/css/flag-icon.min.css",
-                      "~/Content/css/cs-skin-elastic.css",
-                      "~/Content/scss/style.css"));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
